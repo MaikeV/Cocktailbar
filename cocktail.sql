75047a88f794a48520abb057383535b883a3535c
Unknown <pini2911@hotmail.de>
2017-12-14 14:23:03 +0100

    Fixed Database

--- cocktail.sql
+++ cocktail.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 07. Dez 2017 um 14:18
+-- Erstellungszeit: 14. Dez 2017 um 14:21
 -- Server-Version: 10.1.26-MariaDB
 -- PHP-Version: 7.1.8
 
@@ -44,7 +44,8 @@ CREATE TABLE `t_cocktail` (
   `U_ID` int(11) NOT NULL,
   `Alcohol` tinyint(1) NOT NULL DEFAULT '1',
   `Recipename` varchar(256) NOT NULL,
-  `CocktailPic` varchar(256) DEFAULT NULL
+  `CocktailPic` varchar(256) DEFAULT NULL,
+  `Recipe` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -57,17 +58,6 @@ CREATE TABLE `t_contains` (
   `I_ID` int(11) NOT NULL,
   `C_ID` int(11) NOT NULL,
   `Quantity` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `t_createsingredient`
---
-
-CREATE TABLE `t_createsingredient` (
-  `I_ID` int(11) NOT NULL,
-  `U_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -90,7 +80,8 @@ CREATE TABLE `t_has` (
 CREATE TABLE `t_ingredient` (
   `I_ID` int(11) NOT NULL,
   `Ingredient` varchar(256) NOT NULL,
-  `UN_ID` int(11) NOT NULL
+  `UN_ID` int(11) NOT NULL,
+  `U_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -117,6 +108,20 @@ CREATE TABLE `t_unit` (
   `Description` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Daten für Tabelle `t_unit`
+--
+
+INSERT INTO `t_unit` (`UN_ID`, `Description`) VALUES
+(1, 'ml'),
+(2, 'l'),
+(3, 'dl'),
+(4, 'g'),
+(5, 'mg'),
+(6, 'kg'),
+(7, 'Teelöffel'),
+(8, 'Esslöffel');
+
 -- --------------------------------------------------------
 
 --
@@ -138,8 +143,8 @@ CREATE TABLE `t_user` (
 --
 
 INSERT INTO `t_user` (`U_ID`, `Firstname`, `Lastname`, `Password`, `Username`, `Mail`, `Picture`) VALUES
-(8, 'Patrick', 'Piernikarczyk', '123456', 'Pini', 'patrick-piernikarczyk@gmx.de', ''),
-(9, 'Ulrike', 'Guljasch', 'blabla', 'Uli', 'u.guljasch@web.de', '');
+(10, 'Maike', 'Voss', '753692ec36adb4c794c973945eb2a99c1649703ea6f76bf259abb4fb838e013e', 'Maike', 'maike.voss@gmx.de', ''),
+(11, 'Patrick', 'Piernikarczyk', '753692ec36adb4c794c973945eb2a99c1649703ea6f76bf259abb4fb838e013e', 'Pini', 'patrick-piernikarczyk@gmx.net', '');
 
 --
 -- Indizes der exportierten Tabellen
@@ -166,13 +171,6 @@ ALTER TABLE `t_contains`
   ADD KEY `I_ID` (`I_ID`);
 
 --
--- Indizes für die Tabelle `t_createsingredient`
---
-ALTER TABLE `t_createsingredient`
-  ADD KEY `I_ID` (`I_ID`),
-  ADD KEY `U_ID` (`U_ID`);
-
---
 -- Indizes für die Tabelle `t_has`
 --
 ALTER TABLE `t_has`
@@ -184,7 +182,8 @@ ALTER TABLE `t_has`
 --
 ALTER TABLE `t_ingredient`
   ADD PRIMARY KEY (`I_ID`),
-  ADD KEY `UN_ID` (`UN_ID`);
+  ADD KEY `UN_ID` (`UN_ID`),
+  ADD KEY `U_ID` (`U_ID`);
 
 --
 -- Indizes für die Tabelle `t_rated`
@@ -227,10 +226,15 @@ ALTER TABLE `t_cocktail`
 ALTER TABLE `t_ingredient`
   MODIFY `I_ID` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT für Tabelle `t_unit`
+--
+ALTER TABLE `t_unit`
+  MODIFY `UN_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
 -- AUTO_INCREMENT für Tabelle `t_user`
 --
 ALTER TABLE `t_user`
-  MODIFY `U_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `U_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- Constraints der exportierten Tabellen
 --
@@ -250,13 +254,6 @@ ALTER TABLE `t_contains`
   ADD CONSTRAINT `t_contains_ibfk_2` FOREIGN KEY (`I_ID`) REFERENCES `t_ingredient` (`I_ID`);
 
 --
--- Constraints der Tabelle `t_createsingredient`
---
-ALTER TABLE `t_createsingredient`
-  ADD CONSTRAINT `t_createsingredient_ibfk_1` FOREIGN KEY (`I_ID`) REFERENCES `t_ingredient` (`I_ID`),
-  ADD CONSTRAINT `t_createsingredient_ibfk_2` FOREIGN KEY (`U_ID`) REFERENCES `t_user` (`U_ID`);
-
---
 -- Constraints der Tabelle `t_has`
 --
 ALTER TABLE `t_has`
@@ -267,7 +264,7 @@ ALTER TABLE `t_has`
 -- Constraints der Tabelle `t_ingredient`
 --
 ALTER TABLE `t_ingredient`
-  ADD CONSTRAINT `t_ingredient_ibfk_1` FOREIGN KEY (`UN_ID`) REFERENCES `t_unit` (`UN_ID`),
+  ADD CONSTRAINT `t_ingredient_ibfk_1` FOREIGN KEY (`U_ID`) REFERENCES `t_user` (`U_ID`),
   ADD CONSTRAINT `t_ingredient_ibfk_2` FOREIGN KEY (`UN_ID`) REFERENCES `t_unit` (`UN_ID`);
 
 --
